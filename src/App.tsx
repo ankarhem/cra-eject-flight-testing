eff3e8f612c40914af96af525cd7579833212779
Jakob Ankarhem <jakob.ankarhem@outlook.com>
2023-02-10 11:42:02 +0100

    all tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="text-center">
       <header className="flex h-screen flex-col items-center justify-center bg-slate-800 text-2xl text-white">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={logo}
+          className="pointer-events-none h-1/3 motion-safe:animate-spin"
+          alt="logo"
+          style={{
+            animationDuration: '20s',
+          }}
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-cyan-400"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
