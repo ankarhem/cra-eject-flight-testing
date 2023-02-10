4ed1464314b2b17d924ee535d564ca872381ad7a
Jakob Ankarhem <jakob.ankarhem@outlook.com>
2023-02-10 11:33:28 +0100

    tailwind works

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="text-center">
+      <header className="text-2xl bg-slate-800 h-screen flex items-center flex-col justify-center text-white">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
